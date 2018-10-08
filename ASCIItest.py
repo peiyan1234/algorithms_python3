c427ef157c038e3c5d8dcf985433ee551f867120
peiyan1234 <d05548014@ntu.edu.tw>
2018-10-08 12:11:17 +0800

    Add files via upload

---
+++ ASCIItest.py
@@ -0,0 +1,4 @@
+print(">>> ", 0b1011010)
+print(">>> ", ord('Z'))
+print(">>> ", chr(90))
+print(">>> ", chr(90), chr(101), chr(100), chr(32), chr(65), chr(46), chr(32), chr(83), chr(104), chr(97), chr(119))
